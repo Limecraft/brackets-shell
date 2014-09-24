8c1066bc05fdc3efe183441ffeba2224e2aa0489
Karel Braeckman <karel.braeckman@limecraft.com>
2014-09-24 14:17:37 +0200

    use edge / edge-js projects for node / www

--- Gruntfile.js
+++ Gruntfile.js
@@ -214,7 +214,7 @@ module.exports = function (grunt) {
         },
         "git": {
             "www": {
-                "repo"      : grunt.option("www-repo") || "../local-client-js",    // TODO user configurable?
+                "repo"      : grunt.option("www-repo") || "../edge-js",    // TODO user configurable?
                 "branch"    : grunt.option("www-branch") || "development"
             },
             "shell": {
@@ -233,10 +233,10 @@ module.exports = function (grunt) {
             "version"       : "1.2.11"
         },
         "limecraft-node": {
-            "repo": grunt.option("limecraft-node-repo") || "../barracuda"
+            "repo": grunt.option("limecraft-node-repo") || "../edge"
         },
         "limecraft-release-node": {
-            "repo": grunt.option("limecraft-node-repo") || "../barracuda"
+            "repo": grunt.option("limecraft-node-repo") || "../edge"
         }
     });
 
