471c76059312a54dfe03f5d96947353e352576e2
Karel Braeckman <karel.braeckman@limecraft.com>
2015-02-18 16:59:32 +0100

    fix: the vcx project is not called brackets.vcxproj any longer

--- scripts/fix-msvc.sh
+++ scripts/fix-msvc.sh
@@ -1,2 +1,2 @@
-cat brackets.vcxproj | sed 's.xcopy \\efy.xcopy /efy.g'  > brackets.vcxproj.txt
-mv brackets.vcxproj.txt brackets.vcxproj
+cat Field\ Dock.vcxproj | sed 's.xcopy \\efy.xcopy /efy.g'  > fielddock.vcxproj.txt
+mv fielddock.vcxproj.txt Field\ Dock.vcxproj
